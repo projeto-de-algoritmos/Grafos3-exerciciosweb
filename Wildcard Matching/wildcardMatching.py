0477d18fbd5495b950c8db10864942d0dca88847
pedroblome <joaopedroblome78@gmail.com>
2023-05-28 00:03:26 -0300

    wildcard matching

---
+++ Wildcard Matching/wildcardMatching.py
@@ -0,0 +1,37 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        dp[0][0] = True
+
+        for j in range(1, n + 1):
+            if p[j - 1] == '*':
+                dp[0][j] = dp[0][j - 1]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if p[j - 1] == '?' or p[j - 1] == s[i - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif p[j - 1] == '*':
+                    dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
+
+        return dp[m][n]
+
+
+# Test cases
+solution = Solution()
+
+# Example 1
+s = "aa"
+p = "a"
+print(solution.isMatch(s, p))  # Output: False
+
+# Example 2
+s = "aa"
+p = "*"
+print(solution.isMatch(s, p))  # Output: True
+
+# Example 3
+s = "cb"
+p = "?a"
+print(solution.isMatch(s, p))  # Output: False
